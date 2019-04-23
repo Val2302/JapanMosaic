796ab72ab7c03d80f1dcb768a9eaa6269eb7b152
Valery Aucharenka <Valery_Aucharenka@epam.com>
2019-04-23 18:22:36 +0300

    d

---
+++ JapanMosaic/ArrayConverter.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace JapanMosaic
+{
+	public static class ArrayConverter
+	{
+		private const int increment = 0;
+
+		public static string Convert<T> ( T[ ] array, Func<T, char> convert = null )
+		{
+			if ( array is null )
+			{
+				throw new ArgumentNullException( @"In ArrayConverter.Convert() argument ""array"" is '=' null" );
+			}
+
+			var headerNumbers = GenerateHeaderNumbers( array.Length );
+			var dataRow = convert is null ? GenerateRow( ) : GenerateConvertRow( );
+			var bottomLine = GenerateBottomLine( array.Length );
+
+			return headerNumbers + dataRow + bottomLine;
+
+			string GenerateRow ( )
+			{
+				var row = "║ ║";
+
+				foreach ( var item in array )
+				{
+					row += item + "│";
+				}
+
+				row = row.Substring( 0, row.Length - 1 ) + "║\n";
+
+				return row;
+			}
+
+			string GenerateConvertRow ( )
+			{
+				var row = "║ ║";
+
+				foreach ( var item in array )
+				{
+					row += convert( item ) + "│";
+				}
+
+				row = row.Substring( 0, row.Length - 1 ) + "║\n";
+
+				return row;
+			}
+		}
+
+		public static string Convert<T> ( T[ , ] array, Func<T, char> convert = null )
+		{
+			if ( array is null )
+			{
+				throw new ArgumentNullException( @"In ArrayConverter.Convert() argument ""array"" is '=' null" );
+			}
+
+			var headerNumbers = GenerateHeaderNumbers( array.Length );
+			var dataRow = convert is null ? GenerateRows( ) : GenerateConvertRows( );
+			var bottomLine = GenerateBottomLine( array.Length );
+
+			return headerNumbers + dataRow + bottomLine;
+
+			string GenerateRows ( )
+			{
+				var row = "║ ║";
+
+				foreach ( var item in array )
+				{
+					row += item + "│";
+				}
+
+				row = row.Substring( 0, row.Length - 1 ) + "║\n";
+
+				return row;
+			}
+
+			string GenerateConvertRows ( )
+			{
+				var row = "║ ║";
+
+				foreach ( var item in array )
+				{
+					row += convert( item ) + "│";
+				}
+
+				row = row.Substring( 0, row.Length - 1 ) + "║\n";
+
+				return row;
+			}
+		}
+
+		private static string GenerateHeaderNumbers ( int length )
+		{	
+			var upperLine = "╔═";
+			var middleLine = "║ ";
+			var lowerLine = "╠═";
+
+			for ( int i = 0; i < length; i++ )
+			{
+				upperLine += "╦═";
+				middleLine += "║" + ( i + increment ) % 10;
+				lowerLine += "╬═";
+			}
+
+			upperLine += "╗\n";
+			middleLine += "║\n";
+			lowerLine += "╣\n";
+
+			return upperLine + middleLine + lowerLine;
+		}
+
+		private static string GenerateBottomLine ( int length )
+		{	
+			var line = "╚═";
+
+			for ( int i = 0; i < length; i++ )
+			{
+				line += "╩═";
+			}
+
+			line += "╝\n";
+
+			return line;
+		}
+	}
+}
--- JapanMosaic/Program.cs
+++ JapanMosaic/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace JapanMosaic
 {
@@ -10,6 +11,10 @@ namespace JapanMosaic
 	{
 		static void Main ( string[ ] args )
 		{
+			var a = new[ ] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+			var t = ArrayConverter.Convert( a, c => c % 2 == 0 ? 'T' : 'F' );
+			WriteLine( t );
+			ReadKey( );
 		}
 	}
 }

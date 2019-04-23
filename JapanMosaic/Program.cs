c831ddf6a416849515b95f924927fedf1cafb8bc
Valery Aucharenka <Valery_Aucharenka@epam.com>
2019-04-23 14:40:51 +0300

    Add project files.

---
+++ JapanMosaic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JapanMosaic
+{
+	class Program
+	{
+		static void Main ( string[ ] args )
+		{
+		}
+	}
+}
